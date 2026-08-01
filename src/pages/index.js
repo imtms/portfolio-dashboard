8a210b44449f6555ea3aabf3ffd2ccf010485041
root <root@ip-172-26-7-45>
2026-08-01 13:02:57 +0800

    feat: update portfolio research for August 2026

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,7 +81,7 @@ export default function Home() {
     performance: chart.length,
     stockHoldings: stockHoldings.length,
     currencyHoldings: currencyHoldings.length,
-    research: "30 Jul",
+    research: "01 Aug",
   };
 
   const renderContent = () => {
