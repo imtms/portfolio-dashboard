2bdde1710fc7520b3d72e343ac49421b38dedc8f
TMs <tms@imtms.com>
2026-09-01 18:02:25 +0800

    Update portfolio research for September 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -81,7 +81,7 @@ export default function Home() {
     performance: chart.length,
     stockHoldings: stockHoldings.length,
     currencyHoldings: currencyHoldings.length,
-    research: "22 Aug",
+    research: "01 Sep",
   };
 
   const renderContent = () => {
